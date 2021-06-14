c590ea6314fa9eee5cf0849c7bd5f52dbb57866e
yang <kiki9510@gmail.com>
2021-06-14 23:14:58 +0900

    Added python encoding&decoding.

---
+++ etc/encoding.py
@@ -0,0 +1,6 @@
+username = '숮이  💄💅👡👠🎀👙🌂👗🌂🎀💋💌'
+username2 = '헤이즈 ,, 다혜. ☃️'
+encode_username = username2.encode('utf-8')
+decode_username = encode_username.decode('utf-8')
+print(len(encode_username))
+"""Mysql Varchar 데이터 타입의 최대 크기는 255이다."""
